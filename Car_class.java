da660f316a6ebed862eaf978c131c12cb232d6cf
Le Hoang Nhan <46989247+le-hoang-nhan@users.noreply.github.com>
2019-04-23 17:07:37 +0200

    Create Car_class.java

---
+++ Car_class.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+
+public class Car {
+
+
+    double averageMilesPerGallon;
+    String licensePlate;
+    Color paintColor;
+    boolean areTailingWorking;
+
+    public Car(double inputAverageMPG,
+               String inputLicensePlate,
+               Color inputPaintColor,
+               boolean inputAreTaillightsWorking) {
+        this.averageMilesPerGallon = inputAverageMPG;
+        this.licensePlate = inputLicensePlate;
+        this.paintColor = inputPaintColor;
+        this.areTailingWorking = inputAreTaillightsWorking;
+    }
+
+    public void changePaintColor(Color newPaintColor) {
+        this.paintColor = newPaintColor;
+    }
+
+    public double speedingUp(double currentSpeed) {
+        currentSpeed += 100;
+        return currentSpeed;
+    }
+
+}
